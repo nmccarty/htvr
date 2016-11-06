b6bcb056e29eb35373db30e8e43130553ca6fd3b
Nathan McCarty <nathan.mccarty@uky.edu>
2016-11-06 13:56:10 -0600

    moar snowdens

--- Assets/HTVR/Scripts/EnemyScript.cs
+++ Assets/HTVR/Scripts/EnemyScript.cs
@@ -10,12 +10,17 @@ public class EnemyScript : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
+        float x = Random.Range(-100, 100)/100.0f;
+        float y = Random.Range(-100, 100) / 100.0f;
+        float z = Random.Range(-100, 100) / 100.0f;
+
+        Vector3 p = new Vector3(x, y, z);
+        gameObject.transform.position += p;
 	}
 
     void OnCollisionEnter(Collision collision)
     {
-        
-        Destroy(gameObject);
+        if(gameObject.tag == "Enemy(Clone)" && collision.gameObject.tag != "Enemy(Clone)")
+            Destroy(gameObject);
     }
 }
